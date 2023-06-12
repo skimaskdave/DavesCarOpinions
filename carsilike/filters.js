a656f5a3d31c08d75f4af79d4465925d17b41a47
skimaskdave <davidtgrasmeder@gmail.com>
2023-06-12 19:15:36 +0100

    adding a sidebar

---
+++ carsilike/filters.js
@@ -0,0 +1,9 @@
+function openFilters() {
+	document.getElementById().style.width = "20em";
+	document.getElementById().style.marginLeft = "20em";
+}
+
+function closeFilters() {
+	document.getElementById().style.width = "0";
+	document.getElementById().style.marginLeft = "0";
+}
