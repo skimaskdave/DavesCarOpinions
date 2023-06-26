10c389880491cfd8cdf0e07575ef59cdf6a47986
skimaskdave <davidtgrasmeder@gmail.com>
2023-06-27 00:02:54 +0100

    fixed side bar and started working on filters

--- carsilike/filters.js
+++ carsilike/filters.js
@@ -1,11 +1,35 @@
+var allFilters = {
+	// type filters
+	"coupe": false,
+	"convertible": false,
+	"hatchback": false,
+	// gearbox filters
+	"manual": false,
+	"automatic": false
+};
+
 function openFilters() {
 	document.getElementById("myFilters").style.width = "20em";
 	document.getElementById("main").style.marginLeft = "20em";
 	document.getElementById("main").style.display = "none";
+	document.getElementById("page").style.marginLeft = "20em";
+	document.getElementById("myFilters").style.borderRight = "0.25em solid black";
 }
 
 function closeFilters() {
 	document.getElementById("myFilters").style.width = "0";
 	document.getElementById("main").style.marginLeft = "0";
 	document.getElementById("main").style.display = "block";
+	document.getElementById("page").style.marginLeft = "0";
+	document.getElementById("myFilters").style.borderRight = "none";	
+}
+
+function updateFilters(id) {
+	var filterID = id.slice(0, -3);
+	allFilters[filterID] = document.getElementById(id).checked;
+	console.log(allFilters);
+}
+
+function applyFilters() {
+	
 }
